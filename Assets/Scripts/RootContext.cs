33631258ff784ba80e413df0488e8c6f70ef7a12
Vincent Deloso <vincentdeloso@gmail.com>
2016-10-16 16:35:36 -0600

    Added the new managers to the root context.

--- Assets/Scripts/RootContext.cs
+++ Assets/Scripts/RootContext.cs
@@ -21,12 +21,26 @@ public class RootContext : MVCSContext, IRootContext
         base.mapBindings();
 
         GameObject managers = GameObject.Find("Managers");
+        GameObject UI = GameObject.Find("UI");
 
         injectionBinder.Bind<IRootContext>().ToValue(this).ToSingleton().CrossContext();
 
         EventManager eventManager = managers.GetComponent<EventManager>();
         injectionBinder.Bind<IEventManager>().ToValue(eventManager).ToSingleton().CrossContext();
 
+        //The following are dependent on the Event Manager
+        TickTockManager tickTockManager = managers.GetComponent<TickTockManager>();
+        injectionBinder.Bind<ITickTockManager>().ToValue(tickTockManager).ToSingleton().CrossContext();
+
+        CropManager cropManager = managers.GetComponent<CropManager>();
+        injectionBinder.Bind<ICropManager>().ToValue(cropManager).ToSingleton().CrossContext();
+
+        SelectionManager selectionManager = managers.GetComponent<SelectionManager>();
+        injectionBinder.Bind<ISelectionManager>().ToValue(selectionManager).ToSingleton();
+
+        //The following are dependent on the Selection Manager and Crop Manager
+        UIPanelManager panelManager = UI.GetComponent<UIPanelManager>();
+        injectionBinder.Bind<IUIPanelManager>().ToValue(panelManager).ToSingleton();
     }
 
     public void Inject(Object o)
