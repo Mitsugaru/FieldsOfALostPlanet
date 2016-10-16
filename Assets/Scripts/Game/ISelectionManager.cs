720c9c81910dd14f5391b6a5b62ad58a5091214e
Vincent Deloso <vincentdeloso@gmail.com>
2016-10-16 16:32:42 -0600

    Add initial selection manager.

---
+++ Assets/Scripts/Game/ISelectionManager.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+public interface ISelectionManager {
+
+	GameObject Selected { get; }
+}
---
+++ Assets/Scripts/Game/SelectionManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using strange.extensions.mediation.impl;
+
+public class SelectionManager : View, ISelectionManager
+{
+
+    private GameObject selected;
+    public GameObject Selected
+    {
+        get
+        {
+            return selected;
+        }
+    }
+
+    // Use this for initialization
+    protected override void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchSupported)
+        {
+            HandleTouch();
+        }
+        else
+        {
+            HandleMouseAndKeyboard();
+        }
+    }
+
+    void HandleMouseAndKeyboard()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            selected = DetectTileTerrainHit(ray);
+        }
+    }
+
+    void HandleTouch()
+    {
+        // Look for all fingers
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            Touch touch = Input.GetTouch(i);
+
+            // -- Tap: quick touch & release
+            // ------------------------------------------------
+            if (touch.phase == TouchPhase.Ended && touch.tapCount == 1)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                selected = DetectTileTerrainHit(ray);
+            }
+        }
+    }
+
+    GameObject DetectTileTerrainHit(Ray ray)
+    {
+        GameObject target = null;
+
+        RaycastHit[] hits = Physics.RaycastAll(ray, float.MaxValue, LayerMask.GetMask("TileTerrain"));
+        for (int i = 0; i < hits.Length; i++)
+        {
+            target = hits[i].transform.gameObject;
+        }
+
+        return target;
+    }
+}
