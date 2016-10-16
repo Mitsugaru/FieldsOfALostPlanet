f84a5129c1ef03afc636f9fc576fa49cc5263426
Vincent Deloso <vincentdeloso@gmail.com>
2016-10-16 16:33:12 -0600

    Added temp UI panel manager class.

---
+++ Assets/Scripts/UI/UIPanelManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using strange.extensions.mediation.impl;
+
+public class UIPanelManager : View, IUIPanelManager
+{
+
+    [Inject]
+    public IEventManager EventManager { get; set; }
+
+    [Inject]
+    public ISelectionManager SelectionManager { get; set; }
+
+    [Inject]
+    public ICropManager CropManager { get; set; }
+
+	public GameObject TileInfoPanel;
+
+    private Slider slider;
+
+    // Use this for initialization
+    protected override void Start()
+    {
+        slider = TileInfoPanel.GetComponentInChildren<Slider>();
+        EventManager.AddListener<TickElapsedEvent>(HandleTickElapsed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		TileInfoPanel.SetActive(SelectionManager.Selected != null);
+    }
+
+    private void HandleTickElapsed(TickElapsedEvent e)
+    {
+        slider.value = CropManager.GetCropInfo(null).Growth;
+    }
+}
