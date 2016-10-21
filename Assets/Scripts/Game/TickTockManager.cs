7af6279140141d17efd3fa3eda9d26f361d48ba8
Vincent Deloso <vincentdeloso@gmail.com>
2016-10-21 14:32:14 -0600

    Tick Tock Manager handles the night and day for the global directional light.

--- Assets/Scripts/Game/TickTockManager.cs
+++ Assets/Scripts/Game/TickTockManager.cs
@@ -18,14 +18,29 @@ public class TickTockManager : View, ITickTockManager
 
     public int secondsPerTick = 3;
 
+    public int ticksPerDay = 60;
+
+    public int daysPerSeason = 30;
+
+    public GameObject sun;
+
+    public Vector3 initialRotation = Vector3.zero;
+
     private float timeSinceLastTick = 0;
+    private int currentDay = 0;
+    private int ticksThisDay = 0;
     private float pauseTime = 0;
     private int tickCount = 0;
+
+    private Quaternion previousSunTickRotation;
 
     // Use this for initialization
     protected override void Start()
     {
         base.Start();
+
+        sun.transform.eulerAngles = initialRotation;
+        previousSunTickRotation = sun.transform.rotation;
     }
 
     // Update is called once per frame
@@ -35,11 +50,26 @@ public class TickTockManager : View, ITickTockManager
         {
             float current = Time.time;
             float deltaTime = current - pauseTime - timeSinceLastTick;
+            //Handle sun day / night
+            float lerpDiff = Mathf.Min(deltaTime / secondsPerTick, 1.0f);
+            Quaternion sunQuat = Quaternion.Euler(TargetSunRotation());
+            sun.transform.rotation = Quaternion.Lerp(previousSunTickRotation, sunQuat, lerpDiff);
+
             if (deltaTime >= secondsPerTick)
             {
                 tickCount++;
                 timeSinceLastTick = current;
+                previousSunTickRotation = sunQuat;
                 pauseTime = 0;
+                //Calculate day passage
+                ticksThisDay++;
+                if (ticksThisDay >= ticksPerDay)
+                {
+                    ticksThisDay = 0;
+                    currentDay++;
+                    //TODO send out an event that a day has passed
+                }
+
                 EventManager.Raise(new TickElapsedEvent());
             }
         }
@@ -56,5 +86,23 @@ public class TickTockManager : View, ITickTockManager
             pauseTime = Time.time - pauseTime;
         }
         pause = state;
+    }
+
+    private Vector3 TargetSunRotation()
+    {
+        Vector3 sunEuler = Vector3.zero;
+        float seasonRatio = ((float)(currentDay) / (float)daysPerSeason);
+        sunEuler.y = seasonRatio * 360.0f;
+
+        int futureTick = ticksThisDay + 1;
+        if (futureTick >= ticksPerDay)
+        {
+            futureTick = 0;
+        }
+
+        float dayRatio = (float)futureTick / (float)ticksPerDay;
+        sunEuler.x = (dayRatio * 360.0f);
+
+        return sunEuler;
     }
 }
