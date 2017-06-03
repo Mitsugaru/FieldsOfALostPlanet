b4862b9a501f53825b8595d6a60816072d825284
Vincent <mitsugaru@users.noreply.github.com>
2017-06-03 09:47:47 -0600

    Adjusted hex grid to be row and column based instead of total tiles.

--- Assets/Scripts/HexagonBoard.cs
+++ Assets/Scripts/HexagonBoard.cs
@@ -19,7 +19,9 @@ public class HexagonBoard : MonoBehaviour
 
     public GameObject imagePrefab;
 
-    public int startingCount = 30;
+    public int rows = 15;
+
+    public int columns = 15;
 
     public int offsetX = 0;
 
@@ -45,44 +47,40 @@ public class HexagonBoard : MonoBehaviour
         float posX = 50.0f * scale;
         float posY = 50.0f * scale;
         int count = 0;
-        int row = 1;
-        for (int i = 0; i < startingCount; i++)
+        int currentRow = 1;
+        for (int i = 0; i < rows; i++)
         {
-            GameObject tile = GameObject.Instantiate(imagePrefab);
-            TileInfo info = TileManager.generateTile(tile);
-            SelectInteractionScript dScript = tile.GetComponent<SelectInteractionScript>();
-            RootContext.Inject(dScript);
-            tile.name = "Tile-" + info.id;
-            tile.transform.SetParent(transform);
-            tile.transform.position = new Vector3(posX + offsetX, posY + offsetY, 0);
-            tile.transform.localScale *= scale;
-            Image image = tile.GetComponent<Image>();
-            Sprite sprite = TerrainSpriteManager.retrieveSprite(info.type);
-            if (image != null && sprite != null)
+            for (int j = 0; j < columns; j++)
             {
-                image.sprite = sprite;
-                image.enabled = true;
+                GameObject tile = GameObject.Instantiate(imagePrefab);
+                TileInfo info = TileManager.generateTile(tile);
+                SelectInteractionScript dScript = tile.GetComponent<SelectInteractionScript>();
+                RootContext.Inject(dScript);
+                tile.name = "Tile-" + info.id;
+                tile.transform.SetParent(transform);
+                tile.transform.position = new Vector3(posX + offsetX, posY + offsetY, 0);
+                tile.transform.localScale *= scale;
+                Image image = tile.GetComponent<Image>();
+                Sprite sprite = TerrainSpriteManager.retrieveSprite(info.type);
+                if (image != null && sprite != null)
+                {
+                    image.sprite = sprite;
+                    image.enabled = true;
+                }
+
+                posX += 100.0f * scale;
             }
 
-            posX += 100.0f * scale;
-            if (count == 12)
+            currentRow++;
+            if (currentRow % 2 == 0)
             {
-                count = 0;
-                row++;
-                if (row % 2 == 0)
-                {
-                    posX = 100.0f * scale;
-                }
-                else
-                {
-                    posX = 50.0f * scale;
-                }
-                posY += 74.0f * scale;
+                posX = 100.0f * scale;
             }
             else
             {
-                count++;
+                posX = 50.0f * scale;
             }
+            posY += 74.0f * scale;
         }
     }
 
@@ -90,7 +88,7 @@ public class HexagonBoard : MonoBehaviour
     {
         // Update image to what we're going to terraform to
         TileInfo info = TileManager.getTileInfo(e.TileId);
-        if(info != null)
+        if (info != null)
         {
             Image image = info.gameObject.GetComponent<Image>();
             Sprite sprite = TerrainSpriteManager.retrieveSprite(e.Terrain);
